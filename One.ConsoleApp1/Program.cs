1a20ed5c47ab9293dca05d2dd0f6d45a67424332
aungnyeinchan <aungnyeinchan.dev@gmail.com>
2026-02-12 23:07:01 +0630

    feat: adoService as a share service

---
+++ One.ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+
+using One.ShareProject;
+using System.Data;
+
+string myConnection = "Data Source=.;Initial Catalog=KSLH_01;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True";
+
+AdoDotNetService adoService = new AdoDotNetService(myConnection);
+
+
+string query = @"select * from Tbl_BLogs where Tbl_BLogs.DeleteFlag = @DeleteFlag";
+List<SqlQueryParameter> sqlParameterList = new List<SqlQueryParameter>
+{
+    new SqlQueryParameter {Name = "@DeleteFlag" ,Value = 0}
+};
+
+var blogs = adoService.Query(query,sqlParameterList);
+
+if(blogs is null)
+{
+    Console.WriteLine("Blogs not found!");
+    return;
+}
+
+foreach (DataRow blog in blogs.Rows)
+{
+    Console.WriteLine($"Author Name is {blog["AuthorName"]} \n");
+}
---
+++ One.ShareProject/AdoDotNetService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace One.ShareProject
+{
+    public class AdoDotNetService
+    {
+        private readonly string _connection = "";
+
+        public AdoDotNetService(string connection)
+        {
+            this._connection = connection;
+        }
+
+        public DataTable? Query(string query, List<SqlQueryParameter> parameters)
+        {
+            SqlConnection connection = new SqlConnection(this._connection);
+            SqlCommand cmd = new SqlCommand(query, connection);
+
+
+            foreach (var parameter in parameters)
+            {
+                cmd.Parameters.AddWithValue(parameter?.Name, parameter?.Value);
+            }
+
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+
+            return dt;
+        }
+
+    }
+
+    public class SqlQueryParameter
+    {
+        public string? Name  { get; set; }
+
+        public object? Value { get; set; }
+    }
+
+}
